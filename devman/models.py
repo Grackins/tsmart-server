8ddacba354a136e55273f1bcf15dfb8b5350a80a
Grace Hawkins <example@example.com>
2020-07-24 02:19:58 +0430

    Add weather device model

--- devman/models.py
+++ devman/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import time
 import socket
 
-class SecDevice(models.Model):
+class Device(models.Model):
     ip = models.CharField(
             verbose_name='IP Address',
             max_length=15,
@@ -10,15 +10,6 @@ class SecDevice(models.Model):
     port = models.IntegerField(
             verbose_name='Port Number',
             )
-    status = models.BooleanField(
-            default=False,
-            )
-
-    def toggle_status(self):
-        self.status = not self.status
-        while self.send_message('1' if self.status else '0') != b'OK\n':
-            time.sleep(1)
-        self.save()
 
     def send_message(self, msg):
         host = self.ip
@@ -32,6 +23,18 @@ class SecDevice(models.Model):
         return resp
 
 
+class SecDevice(Device):
+    status = models.BooleanField(
+            default=False,
+            )
+
+    def toggle_status(self):
+        self.status = not self.status
+        while self.send_message('1' if self.status else '0') != b'OK\n':
+            time.sleep(1)
+        self.save()
+
+
 class SecAlarm(models.Model):
     time = models.DateTimeField(
             verbose_name='Time',
@@ -41,3 +44,15 @@ class SecAlarm(models.Model):
             verbose_name='Device',
             on_delete=models.CASCADE
             )
+
+
+class WeatherDevice(Device):
+    humidity = models.FloatField()
+    weather = models.FloatField()
+
+    def update(self):
+        info = self.send_message('Get').decode('utf-8').split()
+        self.humidity = float(info[0])
+        self.weather = float(info[1])
+        self.save()
+
