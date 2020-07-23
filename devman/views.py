384ca45571a6ca6aaa0e2424385509c2f48c4ae7
Grace Hawkins <example@example.com>
2020-07-23 18:38:35 +0430

    Add toggle secdevice view function

--- devman/views.py
+++ devman/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+from .models import SecDevice
+
+@require_http_methods(['GET'])
+def toggle_secdevice_view(request):
+    dev_id = request.GET.get('device', -1)
+    device = get_object_or_404(SecDevice, pk=dev_id)
+    device.toggle_status()
+    return render(request, 'devman/toggle_secdevice.html', {'device': device})
+
