25b1082900bf4b46057d071d2b94f63558890e38
Grace Hawkins <example@example.com>
2020-07-23 18:21:11 +0430

    Add security models

---
+++ devman/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class SecDevice(models.Model):
+    ip = models.TextField(
+            verbose_name='IP Address',
+            max_length=15,
+            )
+    port = models.IntegerField(
+            verbose_name='Port Number',
+            )
+
+
+class SecAlarm(models.Model):
+    time = models.DateTimeField(
+            verbose_name='Time',
+            )
+    device = models.ForeignKey(
+            SecDevice,
+            verbose_name='Device',
+            on_delete=models.Cascade,
+            )
